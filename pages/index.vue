18a1fdf6ed32fd40035dc734d20af04c555a49c4
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-06-16 22:06:02 -0400

    Create index.vue
    Criada index.vue para redirecionar para o login

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+//redirecionando
+await navigateTo('/login', { redirectCode: 302 });
+</script>
+
+<template>
+  <div>
+    Redirecionando para a página de login...
+  </div>
+</template>
