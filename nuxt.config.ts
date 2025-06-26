19b0bd20b5c6425627a8ca8d0578288820dbf504
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-06-25 23:47:04 -0400

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL
+    }
+  }
 })
